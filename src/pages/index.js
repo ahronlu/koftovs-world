5370995ae86ccda72f5f6f1a9434f080fdadbe14
koftov <ahronlu@gmail.com>
2021-05-07 12:42:56 +0300

    getStaticProps

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ export default function Home({ countries }) {
   );
 }
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   const res = await fetch("https://restcountries.eu/rest/v2/all");
   const countries = await res.json();
 
